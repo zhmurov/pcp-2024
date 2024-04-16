b83000ee3b5324135549d21dfa29852d0668d420
Artem Zhmurov <zhmurov@gmail.com>
2024-04-16 14:20:45 +0300

    Print thread number

--- hello.cu
+++ hello.cu
@@ -1,29 +1,12 @@
 #include <stdio.h>
 
+__global__ void gpu_kernel()
+{
+    int i = threadIdx.x + blockIdx.x*blockDim.x;
+    printf("%d\n", i);
+}
+
 int main()
 {
-    int driverVersion = 0;
-    cudaDriverGetVersion(&driverVersion);
-    printf("CUDA driver: %d\n", driverVersion);
-
-    int runtimeVersion = 0;
-    cudaRuntimeGetVersion(&runtimeVersion);
-    printf("CUDA runtime: %d\n", runtimeVersion);
-
-    int         numDevices;
-    cudaError_t stat = cudaGetDeviceCount(&numDevices);
-
-    for (int i = 0; i < numDevices; i++)
-    {
-        cudaDeviceProp prop;
-        stat = cudaGetDeviceProperties(&prop, i);
-
-        printf("%d: %s, CC %d.%d, %d SMs running at %dMHz, %luMB\n", i, prop.name,
-            prop.major, prop.minor,
-            prop.multiProcessorCount,
-            prop.clockRate/1000,
-            prop.totalGlobalMem/1024/1024);
-    }
-
-    return 0;
+    gpu_kernel<<<4, 16>>>();
 }
